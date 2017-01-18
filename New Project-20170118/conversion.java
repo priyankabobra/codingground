0772c9fd6f562d183439881288f6a9fcbd55bf05
priyankabobra <priyankabobra@gmail.com>
2017-01-18 12:01:20 -0500

    Created by Coding Ground Client

---
+++ New Project-20170118/conversion.java
@@ -0,0 +1,23 @@
+//*******************************************************************
+// NOTE: please read the 'More Info' tab to the right for shortcuts.
+//*******************************************************************
+
+import java.lang.Math; // headers MUST be above the first class
+import java.util.Scanner;
+// one class needs to have a main() method
+public class conversion
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+    //OtherClass myObject = new OtherClass("Hello World!");
+    //System.out.print(myObject);
+    double m, f, kg; 
+	double a; 
+	int choice; 
+	System.out.print("Enter a number: "); 
+	Scanner number = new Scanner(System.in); 
+	a = number.nextDouble();
+	
+  }
+}
